ee512d7a6745f91e9432de4864d18da89b002400
Akbar Septriyan <Akbar Septriyan>
2016-12-31 14:44:46 +0700

    refactoring uart

--- interface.py
+++ interface.py
@@ -1,10 +1,10 @@
 import serial
 import time
-ser = serial.Serial('COM4', 9600)  # open serial port
+ser = serial.Serial('COM5', 9600)  # open serial port
 varlala = "___a\nkwkwkw"
-ser.write(str.encode(varlala))
-time.sleep(1)
-varb = "huehuhe"
+# ser.write(str.encode(varlala))
+# time.sleep(1)
+# varb = "huehuhe"
 while ser.inWaiting() > 0:
 	varb = ser.read(1)
 	print(varb)
